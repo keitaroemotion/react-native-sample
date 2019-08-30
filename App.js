3668a137fa0660bbf3031ac8a808861e53392322
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-08-30 12:36:58 +0900

    android launching script added and App.js bit modified

--- App.js
+++ App.js
@@ -6,19 +6,42 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { AppRegistry, Image } from 'react-native';
+import React, { Component }                           from 'react';
+import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
 
-export default class Bananas extends Component {
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
-    );
-  }
+const styles = StyleSheet.create({
+    bigBlue: {
+        color:      'blue',
+        fontWeight: 'bold',
+        fontSize:   30,
+    },
+  
+    red: {
+        color:      'red',
+    },
+});
+
+export default class LotsOfStyles extends Component {
+    render() {
+        return ( 
+            <View>
+                <Text style={styles.red}>just red</Text>
+                <Text style={{width: 50, height: 50, backgroundColor: 'powderblue'}}>boo</Text>
+            </View>
+        );
+    }
 }
+
+// export default class Bananas extends Component {
+//   render() {
+//     let pic = {
+//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     };
+//     return (
+//       <Image source={pic} style={{width: 193, height: 110}}/>
+//     );
+//   }
+// }
 
 // AppRegistry.registerComponent('AwesomeProject', () => Bananas);
 // 
